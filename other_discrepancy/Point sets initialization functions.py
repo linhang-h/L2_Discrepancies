cbc619f2b57eb1989d01dae108c8b3282669d637
Xuanz2537 <xuanz2537@gmail.com>
2025-06-10 04:23:05 -0700

    Update Point sets initialization functions.py

--- other_discrepancy/Point sets initialization functions.py
+++ other_discrepancy/Point sets initialization functions.py
@@ -10,10 +10,16 @@ def getHalton(n, d):
     HaltonGenerator = Halton(dimension=d, randomize=None)
     return HaltonGenerator.gen_samples(n)
 
-# Fibonaccci lattice
+# random in any dim
+def random_nd_points(n, d=5, seed=None):
+    rng = np.random.default_rng(seed)
+    return rng.uniform(0.0, 1.0, size=(n, d))
+
+# Fibonaccci lattice in dim 2
 def fibonacci_rational_lattice(Fn, Fn_minus1):
     i = jnp.arange(Fn, dtype=jnp.float64)
     x = i / Fn
     y = (Fn_minus1 * i) / Fn  # float multiplication and division
     y = y % 1.0               # fractional part in float
     return jnp.stack([x, y], axis=1)
+
